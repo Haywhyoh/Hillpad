3285af8f6245f6014449995a0ee1591f48856fbf
Ayomide <adefeyisayo998@gmail.com>
2023-06-09 19:32:45 +0100

    added the dashboard route

--- Hillpad/src/App.jsx
+++ Hillpad/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/home'
 import Courses from './pages/courses'
+import DashboardStaff from './pages/staff/dashboardStaff'
+import {DisciplineDetails} from './pages/disciplineDetails'
+
 function App() {
   return (
     < div className=''>
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/courses' element={<Courses />} />
+          <Route path='/dashboardStaff' element={<DashboardStaff />} />
+          <Route path='/discipline' element={<DisciplineDetails />} />
         </Routes>
       <Footer />
     </div>
