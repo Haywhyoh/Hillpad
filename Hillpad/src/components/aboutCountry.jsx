1131ce301d990a3f4633f04e6237f1e08b5b5cc7
Ayomide <adefeyisayo998@gmail.com>
2023-07-03 16:03:33 +0100

    created states for different data

---
+++ Hillpad/src/components/aboutCountry.jsx
@@ -0,0 +1,31 @@
+import {FiExternalLink} from 'react-icons/fi'
+
+export default function AboutCountry({props}) {
+    return (
+        <div>
+            <section >
+                <div>
+                    <div className="text-light_black">
+                        <p>{props.about}</p>
+                    </div>
+                    <div>
+                        <a href={props.about_wiki_link} target="_blank" className="text-light my-3 flex gap-2 items-center"><div><FiExternalLink /></div><div>Wikipedia</div></a>
+                    </div>
+                </div>
+                <div>
+                    <div><h2 className="font-bold text-xl mt-6">Trivia and Fun Facts</h2></div>
+                    <div className="text-light font-bold mt-10">Trivia and fun facts about {props.name}</div>
+                    <ul className="text-light_black list-disc mx-8">
+                        <li>After Russia, Canada is the second largest country in the world in terms of land mass.</li>
+                        <li>Canada has more lakes than the rest of the world’s lakes combined. Plenty of opportunities to go for a swim!</li>
+                        <li>The Blackberry Smartphone is developed in Waterloo, Ontario.</li>
+                        <li>Canada is home to approximately 55,000 different insect species.</li>
+                        <li>Canada has a prison for polar bears who break into people’s homes.</li>
+                    </ul>
+                </div>
+            </section>
+
+        </div>
+
+    )
+}
