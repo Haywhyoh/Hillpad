56afa37a1d018fb8d3d39370c052b29c3653e5d7
Ayomide <adefeyisayo998@gmail.com>
2023-07-05 13:00:01 +0100

    added space to search button on iphone

--- Hillpad/src/pages/home.jsx
+++ Hillpad/src/pages/home.jsx
@@ -178,9 +178,12 @@ export default function Home() {
                       </option>
                     </select>
                   </fieldset>
+                  <div className="mx-2">
                   <button className="w-64 xs:w-80 2xs:w-full mb-2 lg:w-full text-white bg-orange py-3 px-2 text-lg font-bold rounded-3xl md:w-28 md:rounded-full">
                     Search
                   </button>
+                  </div>
+                  
                 </form>
               </div>
             </div>
