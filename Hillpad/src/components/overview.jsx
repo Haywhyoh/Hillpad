b8ee99ada7bb83543ac81df0982dcd9cce82dccb
Ayomide <adefeyisayo998@gmail.com>
2023-07-18 09:00:23 +0100

    designed thw overview compomnent

---
+++ Hillpad/src/components/overview.jsx
@@ -0,0 +1,35 @@
+export default function Overview() {
+    return (
+    <div>
+        <div>
+            <h2 className='font-semibold text-black text-2xl my-6'>Course Overview</h2>
+        </div>
+        <div>
+            <p>
+            Electrical engineers shape the technology that powers our world – 
+            from computer systems to electronic devices and power transmission systems. 
+            Apply your skills with an optional paid co-operative internship, 
+            an optional specialization in Management Sciences and leading-edge research projects. 
+            In your final year, you can further specialize in Digital Hardware Design, Communication Systems, 
+            Signal Processing, Control Systems or Power Systems.
+            In April 2022, Ryerson University changed its name to Toronto Metropolitan University. 
+            Learn more about the institution's next chapter
+            </p>
+        </div>
+        <div>
+            <h2 className='font-semibold text-black text-2xl my-6'>Project Structure</h2>
+        </div>
+        <div>
+            <h2 className="text-light text-lg font-semibold">Course include:</h2>
+            <ul className="list-disc px-8">
+                <li>Applied Mathematics</li>
+                <li>Applied Geometry</li>
+                <li>Ghost Trigonmentry</li>
+                <li>Calculus</li>
+                <li>Industrial Physics</li>
+                <li>Fluid Mechanics</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
