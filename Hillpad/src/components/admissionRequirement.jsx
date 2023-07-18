e6e562b4cc17ecf9fed491fc4078dac6637b384e
Ayomide <adefeyisayo998@gmail.com>
2023-07-18 08:55:54 +0100

    created admission req component

---
+++ Hillpad/src/components/admissionRequirement.jsx
@@ -0,0 +1,11 @@
+export default function AdmissionReq() {
+    return(
+        <>
+            <div>
+                <div>
+                    <h2 className='font-semibold text-black text-2xl my-6'>Admission Requirement</h2>
+                </div>
+            </div>
+        </>
+    )
+}
