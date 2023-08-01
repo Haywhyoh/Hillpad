f9b59be876974796def4fe607534b2e1e04afb5f
Ayomide <adefeyisayo998@gmail.com>
2023-08-01 14:47:09 +0100

    created the user card

--- Hillpad/src/components/header.jsx
+++ Hillpad/src/components/header.jsx
@@ -1,7 +1,7 @@
 import logo from '../assets/images/hillpad-transparent.png';
-import  signlogo from '../assets/images/user.png';
-import { FiMenu, FiLayers, FiUser } from 'react-icons/fi';
-import { FaGraduationCap, FaPaperPlane } from 'react-icons/fa';
+import signlogo from '../assets/images/user.png';
+import { FiMenu, FiLayers, FiUser, FiHeart, FiSettings, FiLogOut } from 'react-icons/fi';
+import { FaGraduationCap, FaHeart, FaPaperPlane } from 'react-icons/fa';
 import { LuUser } from 'react-icons/lu';
 import { RiArrowDropDownLine, RiArrowRightSLine } from 'react-icons/ri';
 import { BiWorld, BiBriefcase } from 'react-icons/bi';
@@ -12,41 +12,41 @@ import Login from './login';
 import { IoCloseOutline } from 'react-icons/io5';
 import { useSelector } from 'react-redux';
 import '../components/modal.css'
-export default function Header({props}) {
+export default function Header({ props }) {
     const [modal, setModal] = useState(false);
     const [isHovering, setIsHovering] = useState(false);
     const [isHovering2, setIsHovering2] = useState(false);
     const [scrolled, setScrolled] = useState(false);
 
-  useEffect(() => {
-    window.onscroll = function() {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-  }, []);
+    useEffect(() => {
+        window.onscroll = function () {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+    }, []);
 
     const handleMouseOver = () => {
         setIsHovering(true);
-        
-      };
 
-      const handleMouseOut = () => {
+    };
+
+    const handleMouseOut = () => {
         setIsHovering(false);
-        
-      };
 
-      const handleMouseOver2 = () => {
+    };
+
+    const handleMouseOver2 = () => {
         setIsHovering2(true);
-        
-      };
 
-      const handleMouseOut2 = () => {
+    };
+
+    const handleMouseOut2 = () => {
         setIsHovering2(false);
-        
-      };
+
+    };
     const toggleModal = () => {
         setModal(!modal);
     };
@@ -81,11 +81,16 @@ export default function Header({props}) {
 
         nav.classList.toggle('hidden')
     }
+
     const user = useSelector((state) => state.user);
 
+    function showUser() {
+        let user = document.querySelector('#userCard');
+        user.classList.toggle('hidden');
+    }
 
     return (
-        <header className={ scrolled ? ' w-screen py-4 fixed top-0 bg-white z-40 shadow-md ': ' w-screen py-4 fixed top-0 bg-white z-40' }>
+        <header className={scrolled ? ' w-screen py-4 fixed top-0 bg-white z-40 shadow-md ' : ' w-screen py-4 fixed top-0 bg-white z-40'}>
             <nav className="max-w-full text-sm font-bold mx-auto justify-between hidden lg:flex px-2 xl:px-4 items-center gap-x-4">
                 <div>
                     <Link to='/'>
@@ -95,36 +100,36 @@ export default function Header({props}) {
                     </Link>
                 </div>
                 <div className='flex w-4/5 lg:w-8/12 xl:w-8/12 gap-x-4 items-center ' >
-                    <div className=''onMouseOver={handleMouseOver} >
+                    <div className='' onMouseOver={handleMouseOver} >
                         <div className='flex gap-3 items-center '>
                             <div className='flex gap-3 items-center relative'>
-                            <div className='flex gap-3 items-center hover:text-orange'>
-                                <FiLayers />
-                                <div className='hover:text-orange'>Browse</div>
-                                <div className='text-2xl'><RiArrowDropDownLine /></div>
-                            </div>
-                            { isHovering ?
-                                <div className='absolute top-8 left-0 shadow bg-white w-52  rounded-lg p-4 font-normal ' id='subMenu1'  onMouseOut={handleMouseOut}>
-                                    <Link to='/countries'><div className='flex gap-x-2 border-b border-opacity-10 border-b-light_black py-2 hover:text-orange' onMouseOver={handleMouseOut2} ><BiWorld className='text-xl text-light_black text-opacity-60' /><div>Browse by country</div></div></Link>
-                                    <Link to='/discipline/'><div className='flex gap-x-2 border-b border-opacity-10 border-b-light_black py-2  hover:text-orange' onMouseOver={handleMouseOut2} ><BiBriefcase className='text-xl text-light_black text-opacity-60 ' /><div className='mb-2'>Browse by discipline</div></div></Link>
-                                    <div className='flex gap-x-2 justify-between py-2  hover:text-orange' onMouseOver={handleMouseOver2}>
-                                        <div className='flex gap-x-2 relative' >
-                                            <FaGraduationCap className='text-xl text-light_black text-opacity-60' />
-                                            <div className='' >Programmes</div>
-                                        </div>
-                                        <div className='text-2xl'><RiArrowRightSLine /></div>
-                                    </div>
-                                    {
-                                        isHovering2 ? <div className='absolute  font-normal left-52 p-4 shadow top-28 rounded-md bg-white text-light_black' id='subMenu2' onMouseOver={handleMouseOver2} onMouseOut={handleMouseOut2}>
-                                        <div className='p-1 w-32 hover:text-orange'>Bachelors</div>
-                                        <div className='p-1 w-32 hover:text-orange'>Masters</div>
-                                        <div className='p-1 w-32 hover:text-orange'>Doctorate</div>
-
-                                    </div> : null
-                                    }
-                                    
+                                <div className='flex gap-3 items-center hover:text-orange'>
+                                    <FiLayers />
+                                    <div className='hover:text-orange'>Browse</div>
+                                    <div className='text-2xl'><RiArrowDropDownLine /></div>
                                 </div>
-: null}
+                                {isHovering ?
+                                    <div className='absolute top-8 left-0 shadow bg-white w-52  rounded-lg p-4 font-normal ' id='subMenu1' onMouseOut={handleMouseOut}>
+                                        <Link to='/countries'><div className='flex gap-x-2 border-b border-opacity-10 border-b-light_black py-2 hover:text-orange' onMouseOver={handleMouseOut2} ><BiWorld className='text-xl text-light_black text-opacity-60' /><div>Browse by country</div></div></Link>
+                                        <Link to='/discipline/'><div className='flex gap-x-2 border-b border-opacity-10 border-b-light_black py-2  hover:text-orange' onMouseOver={handleMouseOut2} ><BiBriefcase className='text-xl text-light_black text-opacity-60 ' /><div className='mb-2'>Browse by discipline</div></div></Link>
+                                        <div className='flex gap-x-2 justify-between py-2  hover:text-orange' onMouseOver={handleMouseOver2}>
+                                            <div className='flex gap-x-2 relative' >
+                                                <FaGraduationCap className='text-xl text-light_black text-opacity-60' />
+                                                <div className='' >Programmes</div>
+                                            </div>
+                                            <div className='text-2xl'><RiArrowRightSLine /></div>
+                                        </div>
+                                        {
+                                            isHovering2 ? <div className='absolute  font-normal left-52 p-4 shadow top-28 rounded-md bg-white text-light_black' id='subMenu2' onMouseOver={handleMouseOver2} onMouseOut={handleMouseOut2}>
+                                                <div className='p-1 w-32 hover:text-orange'>Bachelors</div>
+                                                <div className='p-1 w-32 hover:text-orange'>Masters</div>
+                                                <div className='p-1 w-32 hover:text-orange'>Doctorate</div>
+
+                                            </div> : null
+                                        }
+
+                                    </div>
+                                    : null}
                             </div>
 
                         </div >
@@ -165,43 +170,55 @@ export default function Header({props}) {
                     </div>
                 </div>
                 {props.isLoggedIn ? <div className='flex gap-x-3 w-52 justify-between'>
-                    <button className='text-orange flex items-center gap-x-2 relative'><div className='w-8'><img src={signlogo} className='w-8' /></div><div>{ props.userInfo.firstName}</div></button>
-                    <div className='w-60 absolute top-16 right-64 border rounded-md p-2 bg-white'>
-                                    <div className='flex gap-x-4 items-center'>
-                                    <div className='w-8'><img src={signlogo} className='w-8' /></div>
-                                    <div> 
-                                        <div>
-                                            <span>{ props.userInfo.firstName}</span>
-                                            <span>{ props.userInfo.lastName}</span>
+                    <button className='text-orange flex items-center gap-x-2 relative'onClick={showUser} onMouseEnter={showUser}><div className='w-8'><img src={signlogo} className='w-8' /></div><div>{props.userInfo.firstName}</div></button>
+                    <div className='hidden w-70 text-base absolute top-20 right-1/4 deepShadow rounded-md py-2 px-6 bg-white font-normal text-light_black' id='userCard'  >
+                        <div className='flex gap-x-4 items-center py-2'>
+                            <div className='w-8'><img src={signlogo} className='w-8' /></div>
+                            <div>
+                                <div className='font-bold'>
+                                    <span>{props.userInfo.firstName}</span>
+                                    <span>{props.userInfo.lastName}</span>
 
-                                        </div>
-                                        <div className='font-normal'>
-                                        { props.userInfo.email}
-                                        </div>
-                                    </div>
-                                    </div>
+                                </div>
+                                <div className='font-normal'>
+                                    {props.userInfo.email}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex gap-x-4 py-3 border-y-light_black border-opacity-20 border-y'>
+                            <div className='text-xl text-light_black'><FiHeart /> </div>
+                            <div>Wishlist</div>
+                        </div>
+                        <div className='flex gap-x-4 py-3 border-b-light_black border-opacity-20 border-b'>
+                            <div className='text-xl text-light_black'><FiSettings /> </div>
+                            <div>Account Settings</div>
+                        </div>
+                        <div className='flex gap-x-4  pt-6'>
+                            <div className='text-xl text-light_black'><FiLogOut/> </div>
+                            <div>Sign Out</div>
+                        </div>
                     </div>
                     <Link to='/courses'><button className='bg-orange text-white px-4 py-2 rounded-full flex items-center gap-2'><div className='text-md'><FaPaperPlane /></div> <div>Explore</div></button></Link>
-                </div> : 
-                <div className='flex gap-x-3 w-52 justify-between'>
-                <button className='text-orange flex items-center gap-x-2' onClick={toggleModal}><div><LuUser /></div><div>Sign in</div></button>
-                <Link to='/courses'><button className='bg-orange text-white px-4 py-2 rounded-full flex items-center gap-2'><div className='text-md'><FaPaperPlane /></div> <div>Explore</div></button></Link>
-            </div>
+                </div> :
+                    <div className='flex gap-x-3 w-52 justify-between'>
+                        <button className='text-orange flex items-center gap-x-2' onClick={toggleModal}><div><LuUser /></div><div>Sign in</div></button>
+                        <Link to='/courses'><button className='bg-orange text-white px-4 py-2 rounded-full flex items-center gap-2'><div className='text-md'><FaPaperPlane /></div> <div>Explore</div></button></Link>
+                    </div>
                 }
-                
+
 
             </nav>
 
             <nav className='mx-auto px-3 lg:hidden'>
                 <div className='flex justify-between '>
                     <Link to='/'>
-                    <div className='w-20'>
-                        <img src={logo} />
-                    </div>
+                        <div className='w-20'>
+                            <img src={logo} />
+                        </div>
                     </Link>
                     <div className='flex gap-x-6'>
-                    <div className='text-2xl text-light_black font-bold' onClick={toggleModal}><FiUser /></div>
-                    <div className='text-2xl text-light_black font-bold' onClick={toggleHeader}><FiMenu/></div>
+                        <div className='text-2xl text-light_black font-bold' onClick={toggleModal}><FiUser /></div>
+                        <div className='text-2xl text-light_black font-bold' onClick={toggleHeader}><FiMenu /></div>
 
                     </div>
                 </div>
@@ -227,9 +244,9 @@ export default function Header({props}) {
                                     <div className='flex'>
                                         <div className='border-e border-opacity-30 border-light_black'></div>
                                         <div className='px-2'>
-                                        <div className='p-1 w-32 hover:text-orange'>Bachelors</div>
-                                        <div className='p-1 w-32 hover:text-orange'>Masters</div>
-                                        <div className='p-1 w-32 hover:text-orange'>Doctorate</div>
+                                            <div className='p-1 w-32 hover:text-orange'>Bachelors</div>
+                                            <div className='p-1 w-32 hover:text-orange'>Masters</div>
+                                            <div className='p-1 w-32 hover:text-orange'>Doctorate</div>
                                         </div>
                                     </div>
                                 </div>
@@ -246,7 +263,7 @@ export default function Header({props}) {
             {
                 modal && (<div className="modal " > <div onClick={toggleModal}
                     className="overlay " ></div> <div className="modal-content mx-1 w-84 2xs:w-88 sm:w-screen lg:w-fit bg-white" >
-                        <Login handleModal={() => {setModal(!modal)}} />
+                        <Login handleModal={() => { setModal(!modal) }} />
                         <button className="close-modal text-2xl" onClick={toggleModal}> <IoCloseOutline /> </button> </div> </div>
                 )
 
